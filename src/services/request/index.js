fc6be60fb7dc479ff4de3351643ce54747235d81
DWT <2635246427@qq.com>
2022-10-25 13:23:02 +0800

    基础封装demo

---
+++ src/services/request/index.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+import { BASE_URL, TIMEOUT } from "./config"
+
+class DWTRequest {
+    constructor(baseURL, timeout) {
+        this.instance = axios.create({
+            baseURL,
+            timeout
+        })
+
+        this.instance.interceptors.response.use((res) => {
+            return res.data
+        }, err => {
+            return err
+        })
+    }
+  
+    request(config){
+      return this.instance.request(config)
+    }
+
+    get(config){
+      return this.request({...config,method:"get"})
+    }
+
+    post(config){
+      return this.request({...config,method:"post"})
+    }
+
+}
+
+export default new DWTRequest(BASE_URL, TIMEOUT)
