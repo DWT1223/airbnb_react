fa29826833de24e04dbd7a15740a869802dc6674
DWT <2635246427@qq.com>
2022-10-31 20:03:39 +0800

    优化调整

---
+++ src/views/detail/index.jsx
@@ -0,0 +1,25 @@
+import { changeHeaderConfigAction } from '@/store/modules/main'
+import React, { memo, useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import DetailPictures from './c-cpns/detail-pictures'
+import { DetailWrapper } from './style'
+
+const Detail = memo((props) => {
+  const { detailInfo } = useSelector((state) => ({
+    detailInfo: state.detial.detailInfo
+  }), shallowEqual)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(changeHeaderConfigAction({ isFixed: false, isHome: false }))
+  }, [dispatch])
+
+  return (
+    <DetailWrapper>
+      <DetailPictures pictureUrls={detailInfo.picture_urls}/>
+    </DetailWrapper>
+  )
+})
+
+Detail.propTypes = {}
+
+export default Detail
